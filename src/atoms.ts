f3dfd9c5d9ba295699448bc782cd2e5a03a9e448
Ernesto Cullen <ernesto.cullen@environicsanalytics.com>
2024-01-23 18:40:19 -0500

    Added another tool of the same type as a previous one to test the scenario

--- src/atoms.ts
+++ src/atoms.ts
@@ -6,9 +6,10 @@ export const actualStepNumberAtom = atom<number>(0)
 
 export const actualWorkflowAtom = atom<IWorkflow>({
   steps: [
-    {id: 1, toolType: 1, name: 'tool1', params: {param1: 'tool1', ed1: 'config1ed1', filePath: 'one.txt'}},
-    {id: 2, toolType: 2, name: 'tool2', params: {param2: 'tool2', ed1: 'config2ed1', filePath: 'two.xml'}},
-    {id: 3, toolType: 3, name: 'tool3', params: {param3: 'tool3', ed1: 'config3ed1'}},
+    {id: 1, toolType: 1, name: 'tool1', params: {param1: 'tool1, type1', ed1: 'config1ed1', filePath: 'one.txt'}},
+    {id: 2, toolType: 2, name: 'tool2', params: {param2: 'tool2, type2', ed1: 'config2ed1', filePath: 'two.xml'}},
+    {id: 3, toolType: 3, name: 'tool3', params: {param3: 'tool3, type3', ed1: 'config3ed1'}},
+    {id: 4, toolType: 2, name: 'tool4', params: {param2: 'tool4, type2', ed1: 'config4ed1', filePath: 'four.xml'}},
   ]
 })
 
